33f3c416f232c37dad629776b16779b89a9c568f
subrata6630 <subrata6630@gmail.com>
2024-04-18 11:01:17 +0600

    fix layout

--- guest-blogging.php
+++ guest-blogging.php
@@ -1,62 +1,52 @@
 <?php
 
-
 /**
- * Plugin Name: Plugin Name
- * Plugin URI:  Plugin URL Link
+ * Plugin Name: Guest Blogging
+ * Plugin URI:  https://subrata6630.github.io
  * Author:      Plugin Author Name
- * Author URI:  Plugin Author Link
- * Description: This plugin does wonders
+ * Author URI:  https://subrata6630.github.io
+ * Description: This plugin does Gust Blogging
  * Version:     0.1.0
  * License:     GPL-2.0+
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
- * text-domain: prefix-plugin-name
-*/
+ * text-domain: guest-blogging
+ */
 
-
-add_action('ourshortcode', 'call_back_for_our_shortcode');  
-
+add_shortcode('ourshortcode', 'call_back_for_our_shortcode');
 
 function call_back_for_our_shortcode(){
     ?>
-<from class="our_from" method="POST">
+<form class="our_form" method="POST">
     <input type="text" placeholder="Title" name="title"><br>
-    <textarea name="content" id="" cols="30" rows="10">content</textarea>
+    <textarea name="content" id="" cols="30" rows="10">Content</textarea>
     <br>
-    <button type="submit" name="submit">create post</button>
-</from>
+    <button type="submit" name="submit">Create Post</button>
+</form>
 <?php
 
-$titel= $_POST['title'] ?? '';
-$content= $_POST['content'] ??'';
+$title = $_POST['title'] ?? '';
+$content = $_POST['content'] ?? '';
 
 if (isset($_POST['submit'])) {
+    if (!empty($title) && !empty($content)) {
+        $our_post_array = [
+            'post_title'   => $title,
+            'post_content' => $content,
+            'post_status'  => 'draft',
+            'post_author' => 1,
+            'post_type' => 'post',
+        ];
 
-            if (!empty($titel) && !empty('$content')) {
-                $our_post_array = [
-                    'post-title'   => $title,
-                    'post-content' => $content,
-                    'post-status'  => 'draft',
-                    'post-author' => 1,
-                    'post_type' => 'post',
-                ];
-
-                $post_id = wp_insert_post($our_post_array);
-                if ($post_id) {
-                    echo 'post created';
-                } 
-                else{
-                    echo 'post not created';
-                }
-                
-                
-                
-               
-            } 
-  
-} 
-
+        $post_id = wp_insert_post($our_post_array);
+        if ($post_id) {
+            echo 'Post created';
+        } else {
+            echo 'Post not created';
+        }
+    }
 }
+}
+
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
 
 function my_theme_enqueue_scripts() {
